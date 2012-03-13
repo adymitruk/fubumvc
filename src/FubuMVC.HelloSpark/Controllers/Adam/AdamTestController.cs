2aa27883813ae31277e7a2e48b694af2a8446007
Adam Dymitruk <adam@dymitruk.com>
2012-03-13 10:43:58 -0700

    Corrected for proper behaviour. Need to go to success view.

--- src/FubuMVC.HelloSpark/Controllers/Adam/AdamTestController.cs
+++ src/FubuMVC.HelloSpark/Controllers/Adam/AdamTestController.cs
@@ -5,7 +5,7 @@ namespace FubuMVC.HelloSpark.Controllers.Adam
     public class AdamTestController {
         public FubuContinuation post_DoSubmission(AdamInputModel adamInputModel) {
             if (adamInputModel.CreditCard == 0) return FubuContinuation.TransferTo(new AdamResultModel{Message="No credit card number specified"});
-            if (adamInputModel.CreditCard % 2 == 0) return FubuContinuation.TransferTo(new AdamResultModel { Message = "Valid credit card number!" });
+            if (adamInputModel.CreditCard % 2 == 0) return FubuContinuation.TransferTo(new AdamSuccessModel { CreditCard = adamInputModel.CreditCard });
             var successModel = new AdamResultModel { Message = "Please enter an even number!" };
             return FubuContinuation.TransferTo(successModel);
         }
@@ -13,5 +13,17 @@ namespace FubuMVC.HelloSpark.Controllers.Adam
         public AdamViewModel Home(AdamResultModel input){
             return new AdamViewModel {Message = input.Message};
         }
+
+			  public AdamSuccessView post_ShowResult(AdamSuccessModel adamSuccessModel) {
+			  	return new AdamSuccessView {CreditCard = adamSuccessModel.CreditCard};
+			  }
     }
+
+	public class AdamSuccessView {
+		public int CreditCard { get; set; }
+	}
+
+	public class AdamSuccessModel {
+		public int CreditCard { get; set; }
+	}
 }
